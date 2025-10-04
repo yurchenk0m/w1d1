689f36b7089be137cd32e9fe936c8459c3226b35
yurmar <mariya.yurchenko@omron.com>
2025-10-04 22:35:47 +0200

    added __str__ method

--- codes/bank_account.py
+++ codes/bank_account.py
@@ -2,6 +2,9 @@ class BankAccount:
     def __init__(self, owner, balance=0.0):
         self.owner = owner
         self.balance = balance
+
+    def __str__(self):
+        return f"The {self.owner}'s bank account"
     
     def deposit(self, amount):
         if not isinstance(amount, (int, float)):
